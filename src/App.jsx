31adabaea757caaa35b31a5b1fb3ceab3414baee
hiba-hussain2004 <hibavs90@gmail.com>
2026-07-14 19:10:08 +0530

    add Team created

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Addteam from './components/Addteam'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Addteam />} />
+
+
+      </Routes>
       
+      
+      
+      </BrowserRouter>
     </>
   )
 }
